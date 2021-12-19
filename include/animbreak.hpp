9b258db91dddb5f54f03ac5a6d0bb0fa8c422c56
Sonu Aryan <sonuaryan76448002@gmail.com>
2021-12-19 20:34:21 +0530

    front header animbreak.hpp added

---
+++ include/animbreak.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+int get_major_version(){return ANIM_BREAK_STUDIO_MAJOR_VERSION;}
+int get_minor_version(){return ANIM_BREAK_STUDIO_MINOR_VERSION;}
+int get_patch_version(){return ANIM_BREAK_STUDIO_PATCH_VERSION;}
