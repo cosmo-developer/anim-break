c9040fcb017faf72fff5b0908ffb6fc905e3e554
Sonu Aryan <sonuaryan76448002@gmail.com>
2021-12-22 23:05:11 +0530

    file name compatible with linux

--- src/window_manager.cc
+++ src/window_manager.cc
@@ -4,7 +4,7 @@
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 #include <animbreak/log.hpp>
 #include <string>
 #include <thread>
