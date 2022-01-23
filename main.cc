eb443444968e596ca8ed08999c4ad890901e41a5
Sonu Aryan <sonuaryan76448002@gmail.com>
2022-01-23 18:28:02 +0530

    size verified

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 using namespace anim;
 int main(const int argc,const char** argv){
 	anim_window::initialize();
-	anim_window* window=anim_window::create_anim_window("Hello",0,0,100,200);
+	anim_window* window=anim_window::create_anim_window("Hello",100,100,800,600);
 	anim_window::finalize();
 	return 0;
 }
